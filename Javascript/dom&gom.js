9dbe4f37cce3d4b84e3d9e1fab81c543ef7dcc92
nisaict <nisaict0@gmail.com>
2019-02-25 10:59:21 +0600

    js event

---
+++ Javascript/dom&gom.js
@@ -0,0 +1,27 @@
+var redBtnElement = document.getElementById('redBtn');
+
+redBtnElement.onmouseover = function () {
+    var divOneElement = document.getElementById('divOne');
+    divOneElement.style.backgroundColor = 'red';
+}
+
+var redBtnElement = document.getElementById('greenBtn');
+
+redBtnElement.onmouseover = function () {
+    var divOneElement = document.getElementById('divOne');
+    divOneElement.style.backgroundColor = 'green';
+}
+
+var redBtnElement = document.getElementById('blueBtn');
+
+redBtnElement.onmouseover = function () {
+    var divOneElement = document.getElementById('divOne');
+    divOneElement.style.backgroundColor = 'blue';
+}
+
+var redBtnElement = document.getElementById('defaultBtn');
+
+redBtnElement.onmouseover = function () {
+    var divOneElement = document.getElementById('divOne');
+    divOneElement.style.backgroundColor = 'black';
+}
