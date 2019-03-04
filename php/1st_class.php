330694bb3940da1f02befb4e85236528b8198704
TanjimAhmed <tanjimahmed74@gmail.com>
2019-03-04 12:52:14 +0600

    php 1st

---
+++ php/1st_class.php
@@ -0,0 +1,21 @@
+<?php
+
+//$time = date("H");
+//$name = "lol";
+//$msg = 'lllllllllll';
+//
+//
+//if ($name == 'lol'){
+//
+//    $msg = "good";
+//    if ($time>"12"){
+//        $msg = 'bad';
+//    }
+//}
+
+
+
+?>
+
+
+
